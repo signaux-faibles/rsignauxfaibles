e46932e90dc700880151769cb0c6d5f9e6b57c4f
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-07 12:47:35 +0200

    Ajout d'un thème minimal exporté pour les graphiques

--- R/post_theme.R
+++ R/post_theme.R
@@ -1,4 +1,9 @@
-sf_theme <- function(){
-  theme_bw(base_size = 16,
-           base_family = "Quicksand")
+#' Theme for ggplot grapics
+#'
+#' @export
+sf_theme <- function() {
+  ggplot2::theme_bw(
+    base_size = 16,
+    base_family = "Quicksand"
+  )
 }
