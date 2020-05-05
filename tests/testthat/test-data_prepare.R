18b1e0b205fa1896ef98a82c3b790f0861c914e6
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-05-05 19:24:09 +0200

    Correction du test de data prepare
    Test aléatoire, qui fonctionnait sur une ancienne graine.

--- tests/testthat/test-data_prepare.R
+++ tests/testthat/test-data_prepare.R
@@ -114,6 +114,7 @@ create_fte_test_task <- function() {
       replace = TRUE
     )
   )
+  set.seed(234)
   test_task[["train_data"]] <- cbind(test_task[["train_data"]],
     ab = sample(c("a", "b"), nrow(test_task[["train_data"]]), replace = TRUE),
     cd = sample(c("c", "d"), nrow(test_task[["train_data"]]), replace =
@@ -125,6 +126,7 @@ create_fte_test_task <- function() {
 }
 
 create_prepared_fte_test_task <- function() {
+
   fte_test_task <- create_fte_test_task()
 
   prepared_task <- prepare(
@@ -146,15 +148,10 @@ create_prepared_fte_test_task <- function() {
 
 test_that("map creation works as expected", {
   prep_task <- create_prepared_fte_test_task()
-  expect_equal(
+  expect_known_value(
     prep_task[["prepared_validation_data"]],
-    structure(c(0.415763281518593, 0.0886256091762334, 0.0203380864113569,
-        0.784198996145278, 0.010076787089929, 0.425349598750472,
-        0.738037971779704, 0.626527328975499, 0.0963920175563544,
-        0.794141964288428, 0.742424242424242, 0.742424242424242,
-        0.742424242424242, 0.758064516129032, 0.742424242424242, 0.734375,
-        0.734375, 0.765625, 0.734375, 0.734375), .Dim = 5:4, .Dimnames = list(
-        NULL, c("target", "score", "target_encode_ab", "target_encode_cd")))
+    file = "./map_golden.log",
+    update = FALSE
   )
   })
 
