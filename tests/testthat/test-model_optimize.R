a80536ae7810f89063ba0bb803414ebb4a3083d6
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-06-11 15:54:37 +0200

    complete minimal test

--- tests/testthat/test-model_optimize.R
+++ tests/testthat/test-model_optimize.R
@@ -4,7 +4,7 @@ context("Test functions to optimize parameters")
 test_that("optimize_hyperparameters works as expected", {
   task_to_tune <- get_test_task(stage = "train", learner = lrn("classif.rpart"))
 
-  optimize_hyperparameters(
+  task_to_tune <- optimize_hyperparameters(
     task_to_tune,
     param_set = paradox::ParamSet$new(
       list(
@@ -13,5 +13,5 @@ test_that("optimize_hyperparameters works as expected", {
       )
     )
   )
-
+  expect_true("mlr3auto_tuner" %in% names(task_to_tune))
 })
