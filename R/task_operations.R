bc00aebc30e9e98c1c1b0ff217ed8f04c0228562
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-07-24 12:07:59 +0200

    Documentation des nouvelles fonctions

--- R/task_operations.R
+++ R/task_operations.R
@@ -1,11 +1,19 @@
-#' Makes a task for
+#' Copie et nettoie une tâche pour un nouvel entraînement
 #'
-#' @param param
+#' Retourne une tâche d'apprentissage où les éléments du précédent
+#' apprentissage sont nettoyées.
+#' Les données ne sont pas copiées tant qu'elles ne sont pas modifiées.
+#' Attention, une incohérence entre "hist_data" et "mlr3task" ne sera pas
+#' corrigée.
 #'
-#' @return
+#' @param task `sf_task`
+#' @param end_run_status `c("FINISHED", "FAILED", "KILLED")` Si spécifié,
+#' termine le précédent run de mlflow avec le statut donné. Sinon, ne termine
+#' pas le précédent run.
+#' @param keep_resampling `logical(1)` Faut-il garder l'échantillonnage de la
+#' tâche mère ?
+#'
 #' @export
-#'
-#' @examples
 reset_for_new_run <- function(
   task,
   end_run_status = NULL,
@@ -41,6 +49,16 @@ reset_tracker <- function(tracker, end_run_status) {
   return(NULL)
 }
 
+#' Remplace les données historiques par d'autres données
+#'
+#' Reconstitue la tâche d'apprentissage task$mlr3task en conséquence.
+#'
+#' @param task `sf_task`
+#' @param data `data.frame()` Les données, doivent avoir des colonnes "siret",
+#' "periode" et task[["target"]]
+#'
+#' @return La sf_task d'origine
+#' @export
 replace_hist_data <- function(task, data) {
    task <- load_hist_data(
      task,
@@ -51,6 +69,16 @@ replace_hist_data <- function(task, data) {
    return(task)
 }
 
+#' Remplace les données new_data par d'autres données
+#'
+#' @param task `sf_task`
+#' @param data `data.frame()` Les données, doivent avoir des colonnes "siret",
+#' "periode" et task[["target"]]
+#'
+#' @describeIn replace_hist_data
+#'
+#' @return La sf_task d'origine
+#' @export
 replace_new_data <- function(task, data) {
   task <- load_new_data(
     task,
