25567ae91f9cfe0fa55a1cbdc5b466b304f614dd
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-11-19 13:39:07 +0100

    coquille d'une méthode explain

--- R/model_explain.R
+++ R/model_explain.R
@@ -2,11 +2,19 @@
 #'
 #' @inheritParams generic_task
 #' @param explanation_method `function` Fonction qui prend en paramètre:
-#' - "model": `any` le modèle
+#' - "task": `sf_task` la tâche d'apprentissage
 #' - "data_to_explain": `data.frame | data.table`  données à expliquer
 #' @param ... Paramètres additionnels passés à `explanation_method`
 #' @export
-explain.sf_task <- function(task, explanation_method, ...) {
-   return (explanation_method(
+explain.sf_task <- function( #nolint
+                            task,
+                            data_to_explain,
+                            explanation_method = get_default_explanation_method(),
+                            ...) {
+  explanation_method <- get_default_explanation_method()
+
+  return(explanation_method(task$mlr3graph_learner, data_to_explain))
 }
+
+
 
