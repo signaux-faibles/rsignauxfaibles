9de1268313a2280f67ab503f4fa9861dbe8d6711
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-11-20 14:50:53 +0100

    Revert "First try of gam explainer"
    This reverts commit 604a9643e9dc4a200922887104e09231508a4f14.

--- R/model_explain.R
+++ R/model_explain.R
@@ -4,7 +4,6 @@
 #' @param explanation_method `function` Fonction qui prend en paramètre:
 #' - "task": `sf_task` la tâche d'apprentissage
 #' - "data_to_explain": `data.frame | data.table`  données à expliquer
-#' @param data_to_explain `data.frame`
 #' @param ... Paramètres additionnels passés à `explanation_method`
 #' @export
 explain.sf_task <- function( #nolint
@@ -14,49 +13,8 @@ explain.sf_task <- function( #nolint
                             ...) {
   explanation_method <- get_default_explanation_method()
 
-  return(explanation_method(task, data_to_explain, ...))
-}
-
-#' Get default explanation method
-#'
-#' @inheritParams explain.sf_task
-#'
-#' @return `function`
-#' @export
-get_default_explanation_method <- function(task, data_to_explain) {
-   return(explain_gam)
+  return(explanation_method(task$mlr3graph_learner, data_to_explain))
 }
 
 
-#' Get explanations for a gam model
-#'
-#' @inheritParams explain.sf_task
-#'
-#' @return
-#' @export
-#'
-#' @examples
-explain_gam <- function(task, data_to_explain) {
-  prepared_data <- task$mlr3pipeline$predict(data_to_explain)
-  if ("mlr3resample_result" %in% names(task)) {
-    gam_model <- get_gam_from_task(task)
-    explanation <- predict(gam_model, prepared_data, type = "terms")
-  } else if ("mlr3model" %in% names(task)) {
-    # TODO:
-  } else {
-    stop('model should have created "mlr3resample_result" or "mlr3model" property in task during training')
-  }
-  return(explanation)
-}
 
-
-#' Fetches the trained gam model inside a task
-#'
-#' @inheritParams generic_task
-#'
-#' @return `mgcv::gam` gam model
-get_gam_from_task <- function(task) {
-  graphlearner <- task$mlr3resample_result$learners[[1]]
-  gam_model <- graphlearner$model$classif.gam
-  return(gam_model)
-}
