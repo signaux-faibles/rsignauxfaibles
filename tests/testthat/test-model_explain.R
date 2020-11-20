3fed668de82041ca9fc759e39ba9eafc9d198e47
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-11-20 14:51:53 +0100

    Revert "Remove deprecated tests"
    This reverts commit e7bfac43cb7561006d1c0c38494c8f0ea7182fe4.

--- tests/testthat/test-model_explain.R
+++ tests/testthat/test-model_explain.R
@@ -1,7 +1,1 @@
 context("Test explain function")
-
-test_that("explain_gam works on a gam trained on splitted data", {
-  test_task <- get_test_task(stage = "train", learner = mlr3::lrn("classif.gam"))
-  data_to_explain <- head(data.table::as.data.table(test_task$mlr3task$data()))
-  explain_gam(test_task, data_to_explain)
-})
