9385535e9fdeea214efe6a10132a2face8136939
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-11-22 16:44:36 +0100

    Fonctions de chargement et de modification des données de défaillance

--- R/post_prediction_correction.R
+++ R/post_prediction_correction.R
@@ -1,10 +1,116 @@
+#' Requete la base de données pour récupérer les secteurs agrégés
+#'
+#' La collection "secteurs" contient des informations agrégés par code_ape et
+#' periode, pour les entreprises avec un effectif connu supérieur à 10.
+#'
+#' Le mois de janvier 2015 est retiré des données, car ce mois présente un
+#' pic dû au fait qu'on prend en compte les données de défaut URSSAF
+#' uniquement à partir de décembre 2014 (12 mois d'observations pour la
+#' cotisation moyenne), ce qui créé artificiellement des
+#' time_til_outcome == 1 en janvier 2015.
+#'
+#' /!\ Le premier semestre ne dure en conséquence que 5 mois, penser à
+#' redresser en cas d'agrégation.
+#'
+#' @inheritParams generic_task
+#' @param adjust `logical` Faut-il redresser la série ? (anomalies en base,
+#' saisonnalité)
+#' @param ape_to_naf `data.frame` table de passage du code ape au code naf
+#' @param ape_to_secteur `data.frame` table de passage du code ape au secteur
+#'
+#' @return `data.frame` avec les colonnes "code_ape", "periode", "count" qui
+#' correspond au nombre d'observations total, "count_outcome" qui compte le
+#' nombre de défaillances/défauts à 18 mois, "count_new_outcome", qui compte
+#' le nombre de défaillances/défauts à 1 mois, "secteur", "code_naf"
+#'
+#' @export
+requete_secteurs_agreges <- function(
+  task,
+  ape_to_naf = get_ape_to_naf(),
+  ape_to_secteur = get_ape_to_secteur()
+) {
+  require(mongolite)
+  collection <- "secteurs"
+  dbconnection <- mongolite::mongo(
+    collection = collection,
+    db = task$database,
+    url = task$mongodb_uri,
+    verbose = FALSE
+  )
+  secteurs_agreges <- dbconnection$find(
+    "{}",
+    fields = '{"_id.code_ape": 1, "_id.periode": 1, "count": 1, "count_outcome": 1, "count_new_outcome": 1}'
+  )
+  secteurs_agreges$code_ape <- secteurs_agreges[, "_id"]$code_ape
+  secteurs_agreges$periode <- secteurs_agreges[, "_id"]$periode
+  secteurs_agreges[, "_id"] <- NULL
+
+  # Ajout des secteurs
+  secteurs_agreges <- secteurs_agreges %>%
+    left_join(ape_to_secteur)
+
+  # Ajout des naf
+  secteurs_agreges <- secteurs_agreges %>%
+    left_join(ape_to_naf)
+
+  secteurs_agreges <- secteurs_agreges %>%
+    filter(periode > "2015-01-01", periode < "2019-07-01")
+
+  return(secteurs_agreges)
+}
+
+
+#' Recupère la table de correspondance code_ape <-> secteur
+#'
+#' Les secteurs sont utilisés dans les enquêtes de conjoncture de la Banque de
+#' France
+#'
+#' @param path :: chemin d'accès au fichier excel. Par défaut:
+#' "raw_data/RWEBSTATS_table_passage.xlsx". Le fichier est disponible dans
+#' "gollum", sur la page "Ressources externes depuis labtenant dans R".
+#' @param sheet :: Feuille à récupérer, par défaut, la sixième
+#'
+#' @return `data.frame` table de correspondance avec les colonnes "code_ape", "secteur",
+#' "nom_secteur".
+#'
+#' @export
+get_ape_to_secteur <- function(path = file.path("raw_data", "RWEBSTATS_table_passage.xlsx"), sheet = 6) {
+  require(readxl)
+  require(stringr)
+  require(dplyr)
+  ape_to_secteur <- readxl::read_xlsx(path, sheet) %>%
+    select(code_ape = naf700, secteur = emc, nom_secteur = libemc) %>%
+    mutate(secteur = stringr::str_replace_all(str_pad(secteur, 5), " ", "0")) %>%
+    mutate(code_ape = stringr::str_replace(code_ape, fixed("."), ""))
+  return(ape_to_secteur)
+}
+
+#' Récupère la table de correspondance de ape à naf
+#'
+#' Cette table de correspondance doit par défaut se trouver dans le répertoire
+#' "raw_data" et porter le nom "naf5to1.csv".
+#'
+#' Ce fichier est disponible dans le répertoire "gollum", sur la page
+#' "Ressources externes depuis labtenant dans R"
+#'
+#' @param path :: chemin d'accès
+#'
+#' @return `data.frame` table de correspondance avec colonnes "code_ape" et
+#' "code_naf"
+#' @export
+get_ape_to_naf <- function(path = file.path("raw_data", "naf5to1.csv")) {
+  code_ape_to_naf <- readr::read_csv2(path) %>%
+    rename("code_ape" = "n5", "code_naf" = "n1")
+  return(code_ape_to_naf)
+}
+
 #' Fonction de récupération des données de conjoncture Banque de France
 #'
 #' Via l'api webstat.
 #'
 #' La variable webstat_client_ID est indispensable pour s'identifier auprès de
 #' l'api webstat pour faire fonctionner w_series_list. Cf la documentation
-#' officielle ou la page gollum "Accès extérieurs depuis labtenant dans R".
+#' officielle ou la page gollum "Ressources externes depuis labtenant dans R".
 #'
 #' @return `data.frame` Quelques séries de données de conjoncture de la Banque
 #' de France, depuis le premier janvier 2014.
@@ -13,7 +119,7 @@ fetch_conj_data <- function() {
   assertthat::assert_that(exists("webstat_client_ID"),
     msg = paste("L'accès à l'API webstat demande une identification.\n",
       "Veuillez vous référer à la page gollum",
-      '"Accès extérieurs depuis labtenant dans R"')
+      '"Ressources externes depuis labtenant dans R"')
     )
   require(rwebstat)
   require(dplyr)
@@ -101,3 +207,149 @@ check_if_same_string <- function(strings) {
   }
 }
 
+#' Agrège les données de défaillances par secteur
+#'
+#' @param df `data.frame()` Données agrégées.
+#'
+#' @return `data.frame()`
+#' @export
+aggregate_by_secteur <- function(df) {
+  secteurs_agreges <- df %>%
+    group_by(periode, secteur) %>%
+    summarize(
+      count = sum(count),
+      count_new_outcome = sum(count_new_outcome),
+      prop_new_outcome = count_new_outcome / count,
+      nom_secteur = first(nom_secteur),
+      .groups = "drop"
+    )
+  return(secteurs_agreges)
+}
+
+#' Agrège les donnée de défaillances par plages de plusieurs mois
+#'
+#' @param df `data.frame` Données à agréger, avec colonnes "count",
+#' "count_new_outcome", "secteur", "nom_secteur"
+#' @param n_month `1, 2, 3, 6` durée de la plage en mois
+#'
+#' @return `data.frame` Données agrégées, "count" est moyenné,
+#' "count_new_outcome" est sommé, "prop_new_outcome" est recalculé.
+#' @export
+aggregate_by_n_months <- function(df, n_month = 6) {
+  assertthat::assert_that(n_month %in% c(1, 2, 3, 6))
+
+  df <- df %>%
+    mutate(n_month_period = lubridate::floor_date(
+      periode,
+      paste0(n_month, "m")
+    )) %>%
+    add_missing_first_month()
+
+  aggregated_df <- df %>%
+    group_by(secteur, n_month_period) %>%
+    summarise(
+      count = mean(count),
+      count_new_outcome = sum(count_new_outcome),
+      prop_new_outcome = count_new_outcome / count,
+      nom_secteur = first(nom_secteur),
+      .groups = "drop"
+    )
+
+  return(aggregated_df)
+}
+
+#' Fonction utilitaire: corriger l'absence du mois de janvier 2015
+#'
+#' Le mois de janvier est imputé par la moyenne des autresmois de la période.
+#'
+#' @param df `data.frame()`
+#'
+#' @return `data.frame()`
+add_missing_first_month <- function(df) {
+  df <- df %>%
+    group_by(secteur, nom_secteur, n_month_period) %>%
+    tidyr::nest() %>%
+    mutate(
+      data = case_when(
+        n_month_period == "2015-01-01" ~ purrr::map(
+          data,
+          add_missing_first_month_aux
+        ),
+        TRUE ~ data
+      )
+    ) %>%
+    tidyr::unnest("data")
+  return(df)
+}
+
+
+#' Fonction utilitaire: Ajout d'un mois manquant dans le premier trimestre
+#' avec des valeurs moyennes
+#'
+#' Cette fonction est une fonction utilitaire utilisée dans
+#' aggregate_by_n_months, et prend un data.frame obtenu après un
+#' `group_by(secteur, nom_secteur) %>% nest()` sur un data.frame de
+#' défaillances agrégés par secteur. Notamment utilisé dans
+#' aggregate_by_secteur
+#'
+#' @param data_secteur_n_months  `data.frame()` Données sectorielles propres à
+#' un secteur. Colonnes attendues: "periode", "count", "count_new_outcome",
+#' "prop_new_outcome".
+#'
+#' @return le même data.frame avec une ligne pour la période 2015-01-01
+#' moyennée sur les données de la première plage de "n_months" mois.
+#'
+#'
+add_missing_first_month_aux <- function(data_secteur_n_months) {
+  require(dplyr)
+  data_secteur_n_months <- data_secteur_n_months %>%
+    dplyr::add_row(
+      periode = as.Date("2015-01-01"),
+      count = mean(data_secteur_n_months$count),
+      count_new_outcome = mean(data_secteur_n_months$count_new_outcome),
+      prop_new_outcome = count_new_outcome / count
+    ) %>%
+    arrange(periode)
+
+  return(data_secteur_n_months)
+}
+
+#' Correction de la saisonnalité pour le secteur 000C1
+#'
+#' Une forte saisonnalité a été observé pour le secteur 000C1.
+#' Cette fonction corrige cette saisonnalité.
+#'
+#' @param df `data.frame()` Données agrégées par périodes.
+#' @param n_month `int()` Durée de la plage d'agrégation.
+#'
+#' @return
+adjust_seasonnality <- function(df, n_month) {
+  df <- df %>%
+    group_by(secteur) %>%
+    arrange(n_month_period) %>%
+    mutate(n_month_period_int = 1:n()) %>%
+    ungroup()
+
+  df_000C1 <- df %>%
+    filter(secteur == "000C1")
+  trend <- lm(data = df_000C1, prop_new_outcome ~ n_month_period_int)
+
+  n_periods_per_year <- 12 / n_month
+  matrix_size <- ceiling(
+    length(residuals(trend)) / n_periods_per_year
+  ) * n_periods_per_year
+
+  residuals_matrix <- matrix(
+    residuals(trend)[1:matrix_size],
+    ncol = 12 / n_month,
+    byrow = TRUE
+  )
+  seasonality <- residuals_matrix %>% colMeans(na.rm = TRUE)
+
+  df_redr <- df %>%
+    mutate(prop_new_outcome = case_when(
+      secteur == "000C1" ~ prop_new_outcome - seasonality,
+      TRUE ~ prop_new_outcome
+    ))
+  return(df_redr)
+}
